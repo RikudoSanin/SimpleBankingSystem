051e39bd56acbb53c199c3370742655024917f9b
RikudoSanin <srikudo235@gmail.com>
2021-08-24 18:03:03 +0300

    stage 2

--- Simple Banking System/task/src/banking/User.java
+++ Simple Banking System/task/src/banking/User.java
@@ -15,9 +15,30 @@ public class User {
     }
 
     private void generateCardNumber() {
-        userCard += 4000000000000000L + random.nextInt(999999999) +
-                (random.nextInt(10) * 1000000000L);
+        userCard += 4000000000000000L + random.nextInt(999999999) * 10L;
+        userCard += algorithmLuhn();
         userPin = random.nextInt(((9999 - 1000) + 1)) + 1000;
+    }
+
+    private int algorithmLuhn () {
+        int sum = 0;
+        long cloneCard = userCard;
+        long number;
+        for (int i = 0; i < 16; i++) {
+            if (i % 2 != 0) {
+                number = (cloneCard % 10) * 2;
+                number = number >= 10 ? number - 9 : number;
+            } else {
+                number = cloneCard % 10;
+            }
+            sum += number;
+            cloneCard /= 10;
+        }
+        if (sum % 10 == 0) {
+            return 0;
+        } else {
+            return 10 - sum % 10;
+        }
     }
 
     protected long getUserCard() {
